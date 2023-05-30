07fc9f4c8a364e6fa806a04b0e32315152164928
tysonlatek <119722869+tysonlatek@users.noreply.github.com>
2023-05-30 02:18:09 -0400

    VS HTML Assignment

---
+++ VS HTML Assignment/VS HTML Assignment/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function myAnimation() {
+    let id = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
